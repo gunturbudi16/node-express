582244c42e6a6cf168b0b1d64627d800e7feec75
gunturbudi16 <gunturkurniawan411@yahoo.co.id>
2019-09-20 15:52:45 +0700

    fix error heroku

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const app = express();
 const { PORT, secret } = require("./config/envVariables");
 const indexRouter = require("./routes/index");
 
-app.get("/", (req, res) => res.send("Hello Express!"));
 
 const Port = process.env.port || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/env", (req, res) => res.send(JSON.stringify(process.env.SECRET)));
+// app.get("/env", (req, res) => res.send(JSON.stringify(process.env.SECRET)));
+app.use("/", (req, res) => res.send("Hello Express!"));
 app.use("/film", indexRouter);
 
 app.listen(Port, () => console.log(`Server is listening on localhost:${Port}`));
